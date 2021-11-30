11e7fcfe62af453f8905c0278ff80cb3179f112c
João Victor Duarte <joaodicu387@gmail.com>
2021-11-30 20:50:03 -0300

    Ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import router from "./routes/index";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port: Number = 5000;
 
 
